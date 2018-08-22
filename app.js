4ec6c9217ad68e716295d7ac996f081fb244b9af
CesarJC <functioniskey@cesarjcarrillo.com>
2018-08-22 09:20:14 -0700

    Cleaned up code & Error handling
    Cleaned up unused code, and added error handling to app.js

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const expressValidator = require("express-validator");
 const expressSession = require("express-session");
-const { body, validationResult } = require("express-validator/check");
-const { sanitizeBody } = require("express-validator/filter");
 
 const app = express();
 
+// connecting to databse / creating database if it does not exist
 mongoose.connect(
   "mongodb://localhost:27017/contact_db",
   { useNewUrlParser: true }
@@ -20,11 +19,25 @@ app.use(expressValidator());
 app.use(
   expressSession({ secret: "max", saveUninitialized: false, resave: false }) // for sessions
 );
+
 const contactRoutes = require("./routes");
 const managerRoutes = require("./routes/manager");
 
 app.use(contactRoutes);
 app.use(managerRoutes);
+
+// IF page doesn't exist
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render("error", err);
+});
 
 app.listen(3000, () => {
   console.log("server listening on port 3000....");
--- routes/manager.js
+++ routes/manager.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
 const messageModel = require("../models/messages");
 
 router.get("/manager", (req, res) => {
